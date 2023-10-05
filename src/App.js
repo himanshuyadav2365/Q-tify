abba2c34ac23289b6490c76c40caa93dbff93932
Himanshu Yadav <himanshuyadav2365@gmail.com>
2023-10-05 17:15:12 +0530

    Hero section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Hero from './components/Hero/Hero'
+import {fetchTopAlbumns} from './components/api/api'
+import {useEffect, useState} from 'react'
+import Card from './components/Card/Card';
+
 
 function App() {
+  
+  // const [topAlbumData,setTopAlbumData]=useState([])
+
+  // useEffect(()=>{
+  //   generateData()
+  // },[])
+
+  // const generateData = async () => {
+  //   try{
+  //   const data =await fetchTopAlbumns()
+  //   console.log(data)
+  //   setTopAlbumData(data)
+  //   }catch(err){
+  //     console.log(err)
+  //   }
+  // }
+
+  ////why hoisting working in arrow function
+ 
   return (
     <div className="App">
       <NavBar/>
+      <Hero/>
+      {/* {topAlbumData.map((item)=>{
+          return <Card key={item.id} data={item} type="album"/>
+      })} */}
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Card.module.css'
+
+const Card = ({data,type}) => {
+  
+    const getCard=(type)=>{
+        switch(type){
+            case "album" : {
+                const {image ,follows,title,songs}=data;
+                return (
+                    <div className={styles.wrapper}>
+                        <div className={styles.card}>
+                            <img src={image} alt ="album"/>
+                            <div className={styles.banner}>
+
+                            </div>
+                        </div>
+                        <div className={styles.titlewrapper}>
+                            <p>{title}</p>
+                        </div>
+                    </div>
+                )
+            }
+            default: return <></>
+
+        }
+    }
+    return getCard(type)
+}
+
+export default Card
